11c7ea89e835941c4361e9459462d11b82df483f
prakash-pragati <74266386+prakash-pragati@users.noreply.github.com>
2025-03-26 15:43:32 -0500

    Delete lit sample/src directory

--- lit sample/src/event.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Attribute } from "./attribute.interface";
-
-export interface Event {
-    eventInfo : string;
-    category : string;
-    name : string;
-    action : string;
-    attributes : Attribute[]; 
-}
